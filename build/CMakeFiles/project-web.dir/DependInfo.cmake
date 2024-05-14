7b6a2c3f62bec367123b8a07b953d33e64599170
Milansuman <milansuman2016@gmail.com>
2024-05-14 14:27:12 +0530

    initial commit

---
+++ build/CMakeFiles/project-web.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "project-web_autogen/timestamp" "custom" "project-web_autogen/deps"
+  "/home/milan/projects/project-web/browserwindow.cpp" "CMakeFiles/project-web.dir/browserwindow.cpp.o" "gcc" "CMakeFiles/project-web.dir/browserwindow.cpp.o.d"
+  "/home/milan/projects/project-web/components/titlebarbuttons.cpp" "CMakeFiles/project-web.dir/components/titlebarbuttons.cpp.o" "gcc" "CMakeFiles/project-web.dir/components/titlebarbuttons.cpp.o.d"
+  "/home/milan/projects/project-web/main.cpp" "CMakeFiles/project-web.dir/main.cpp.o" "gcc" "CMakeFiles/project-web.dir/main.cpp.o.d"
+  "/home/milan/projects/project-web/build/project-web_autogen/mocs_compilation.cpp" "CMakeFiles/project-web.dir/project-web_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/project-web.dir/project-web_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
